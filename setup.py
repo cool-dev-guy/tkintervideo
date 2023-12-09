90100b243da13774d8dfdc3ee898794802ac1dcb
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2023-12-09 09:05:50 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tkvideo",
+    version="0.0.1",
+    description="A simple Tkinter Python Package For synced audio & video playback.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
