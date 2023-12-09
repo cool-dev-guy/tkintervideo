8ad9ed13bb04092b589f472c243acd243147d328
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2023-12-09 09:55:33 +0530

    chnage to pyproject

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from setuptools import setup, find_packages
+[build-system]
+requires = ["setuptools>=61.0"]
+build-backend = "setuptools.build_meta"
 
-setup(
-    name="tkvideo",
-    version="0.0.1",
-    description="A simple Tkinter Python Package For synced audio & video playback.",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+[project]
+name = "tkintervideo"
+version = "0.0.2"
+authors = [
+  { name="Goutham (cool-guy)" },
+]
+description = "A simple Tkinter Python Package For synced audio & video playback."
+readme = "README.md"
+requires-python = ">=3.6"
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Operating System :: OS Independent",
+]
+
+[project.urls]
+"Homepage" = "https://github.com/cool-dev-guy/tkintervideo"
+"Bug Tracker" = "https://github.com/cool-dev-guy/tkintervideo/issues"
